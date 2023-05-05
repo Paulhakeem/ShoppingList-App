dfb8fa32e97b80c3aa5b56554ab869251f1e7ed5
Paulhakeem <101798876+Paulhakeem@users.noreply.github.com>
2023-05-05 14:11:48 +0000

    my second commit

--- src/views/ShoppingList.vue
+++ src/views/ShoppingList.vue
@@ -33,9 +33,9 @@ watch(items, newItem => {
 </script>
 
 <template>
-  <div class="mx-auto max-w-md rounded-lg place-items-center justify-center grid my-12 bg-gray-200 box-shadow-white shadow-2xl max-h-full">
+  <div class="mx-auto max-w-md rounded-lg place-items-center justify-center grid my-12 bg-gray-200 pb-6">
     
- <img class="w-full" src="../assets/shopping-1.png">
+ <img class="w-full pt-6" src="../assets/shopping-1.png">
     
   <div class=" text-center justify-center p-4 mt-20 ">
     <h1 class="text-2xl font-bold">Create your shopping list with <br><span class="text-blue-500 ">shopping List App</span></h1>
@@ -47,17 +47,43 @@ watch(items, newItem => {
     <button :disabled="newItem.length === 0" class="bg-blue-500 text-white rounded-lg p-2 w-56"> <font-awesome-icon icon="fa-solid fa-circle-plus" /> ADD ITEM</button> 
       
   </form>
-  <ul class="p-4 m-4 gap-2">
+  <transition-group tag="ul" name="list" class="p-4 m-4 gap-2">
     <li 
       v-for="({id, label}, index) in items" 
       :key="id" 
-      class="p-2 font-base m-auto border border-4 bg-white text-xl divide-y-2 place-items-start">
+      class="w-full p-2 font-base m-auto border border-4 bg-white text-xl divide-y-2 place-items-start">
        <font-awesome-icon icon="fa-regular fa-circle-check" class="mr-2 text-blue-500" />
       {{label}}
       <button @click="removeItem"><font-awesome-icon icon="fa-solid fa-circle-xmark" class="text-blue-500"/></button>
     </li>
-  </ul>
+  </transition-group>
    <p v-if="!items.length" class="italic font-bold text-blue-500">You have empty List!</p>
   </div>
   
 </template>
+
+
+<style>
+  .list-enter-from{
+    opacity: 0;
+    transform: scale(0.6);
+  }
+  .list-enter-to{
+    opacity: 1;
+    transform: scale(0);
+  }
+  .list-enter-active{
+    transition: all 0.5s ease;
+  }
+  .list-leave-from{
+    opacity: 1;
+    transform: scale(0);
+  }
+  .list-leave-to{
+    opacity: 0;
+    transform: scale(0.6);
+  }
+  .list-leave-active{
+    transition: all 0.5s ease;
+  }
+  </style>
